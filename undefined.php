f3849419723c6437b076c90d49a3167803a7017b
Jonathan Perez Hernandez <jonathan_perez_hdz@hotmail.com>
2022-04-21 20:44:39 -0500

    undefined.php: Created and modified

---
+++ undefined.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indefinido</title>
+
+    <link rel="stylesheet" href="src/assets/styles/undefined.css">
+    <!-- Latest compiled and minified CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    
+    <?php include_once 'header.php'; ?> 
+
+    <main id="main">
+
+        <div class="mt-4 p-5 bg-danger text-white rounded">
+            <h1>Rol indefinido</h1>
+            <p>La página para este usuario aún no está definida, póngase en contacto con su administrador.</p>
+            <a href="API/sessions/logout.php" class="text-white">Cerrar sesión</a>
+        </div>
+        
+    </main>
+    
+</body>
+</html>
+
